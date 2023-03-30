b9258a7d37604930daef69da9a298edff287a1f1
Josh Bello <superturboturtle@gmail.com>
2023-03-30 13:29:17 -0600

    Pic Fix

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -9,10 +9,7 @@ function Home() {
         <p>1. Browsing Joel's personal library of fine feature films</p>
         <p>2. Listening to Joel's fire podcast.</p>
         <p>3. Getting a good look at this handsome G.</p>
-        <img
-          src="https://dl.boxcloud.com/api/2.0/internal_files/908099177363/versions/978098006963/representations/jpg_paged_2048x2048/content/1.jpg?access_token=1!VpWUS7Qce1hL5bCI8FhQW9iDyLVbGw54A2_DAuA1Gx5-wligaBxmG670mKFFg3JPBrv5TTsZIknCYFS9eTIWw12LY0A_I5w3CG_Y_hk_xS5nNjXg45uDNycmf5pLs0eYyZcjlf5WoqAwMnVDiMqW3tJIUIE1rllbU6EesG2RL0JZU9CoFC0dlPCLEEghQ5yw3P3-2ItxmBXkqYIoi_joM9QZErs8hknPI9Ydj1Bh_gEW1hdpdG30UtOAzM7vpXVeBTuBh73TnoTQYwS_8Sr91KCV2hoCJPBf1toA0LtZsoFaGzvYJ5SsWmmwWBz23KtVmkh7vGI2xdRcxZc11nW5hTD7Zr034nRVY_vNcnL2cF9EYj8xMrvLgLuB0gvv5YYzDi83vR4mPenfMavQQLE_GxhSFCAjly1AMTs6cz6iZUENpDHQv_nW9sHXpkUV72jL6sniwfdkNA-cnlwuPR_ztfyj_ZVsIjYn30lZ55dSrYnI3vDpKCrfzoX_O_MuOkdh2QfkU-1EKG-MftzFY4uMfmtfSuILoluIgZV5dAZv5TpiB98d3bhslJv6JJ-tBQika65A1nP7SOiy9z4Swl56oIKIJ5M0ra05A8SYLX-sY8E80i6UFmmDWFdgAL_6Z6_FkDifKgUhGeJ7UcBgUnqS1I4klhTNyqjblOg89dn53b1BW-BlOnkbMbEtzgVpU8CRPqeMmF7-3eKBI34YUeg3pxbbeVkzliFHUArrIz1odUlrd4_R_w..&shared_link=https%3A%2F%2Fbyu.app.box.com%2Fs%2F8sjz2qei13h4nnlw0e6gc10ot35octkf&box_client_name=box-content-preview&box_client_version=2.90.0"
-          alt="Joel"
-        ></img>
+        <img src="./Joel.jpg" alt="Joel"></img>
       </div>
     </>
   );
